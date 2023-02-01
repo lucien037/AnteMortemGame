b6df1a139b993b2069dde32a5abd071a53825cb5
V̸̡͈̠̺͕̖͇͕͇̻̳͔̦̎̇̐̐̂ͅė̵̛̛͉̲͇̭̹͙́̑̈̋̉͛̆̄̍͘͝͝N̶̡̛̞̫͕̲̱̞̟̼̫͚͉̯̍́̄͐̉̾̈́́ͅỏ̵̜̮͚̯̫̝̬̖̩̾M̷̛̛̰͈̥̰̣͈͎̝͍̬͙̖͇͒̐̍̒͌̀͌̂̿̚͠͝ <87692048+Cantin-L@users.noreply.github.com>
2023-02-01 10:23:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,31 +6,37 @@ LARGEUR_ECRAN = 800
 HAUTEUR_ECRAN = (LARGEUR_ECRAN*0.8)
 
 ecran = pygame.display.set_mode((LARGEUR_ECRAN,HAUTEUR_ECRAN))
-pygame.display.set_caption('TEST')
-
+pygame.display.set_caption('Ante Mortem')
+icon = pygame.image.load('ressources/images/hero.png')
+pygame.display.set_icon(icon)
 
 class personnage(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,x,y,echelle):
+        pygame.sprite.Sprite.__init__(self)
+        image = pygame.image.load('ressources/images/hero.png')
+        # Modifier l'échelle de l'image ex : augmenter taille du personnage
+        self.image = pygame.transform.scale(image, (int(image.get_width() * echelle), int(image.get_height() * echelle)))
+        self.rectangle = self.image.get_rect()
+        self.rectangle.center = (x,y)
 
+    def affiche(self):
+        ecran.blit(self.image, self.rectangle)
+
+perso1 = personnage(150,150,2)
 
 # Ajouter une image dans la fenêtre en fonction des coordonnées x,y
 x = 200
 y = 200
 echelle = 2
-image = pygame.image.load() #ajouter le lien de l'image dans les ()
-# Modifier l'échelle de l'image ex : augmenter taille du personnage
-image = pygame.transform.scale((image, image.get_width()*echelle), (image, image.get_height * echelle))
-# rectangle = image.get_rect()
-# rectangle.center = x,y
 
 # Génerer la fenêtre de jeu
+
 run = True
 while run:
-    # ecran.blit(imge,rectangle)
-
-    for action in pygame.action.get():
+    perso1.affiche()
+    for event in pygame.event.get():
         # Quitter le jeu
-        if action.type == pygame.quit():
+        if event.type == pygame.QUIT:
             run = False
     pygame.display.update()
 pygame.quit()
